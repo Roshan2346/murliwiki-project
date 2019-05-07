d63cccaa4c677910125679cdc9f7010a98c7da57
Roshan Yadav <djrshn2346@gmail.com>
2019-05-07 17:58:17 +0530

    Added in settings and all the fields

--- results/models.py
+++ results/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Results(models.Model):
+	date = models.CharField(max_length=255)
+	title = models.CharField(max_length=255)
+	month = models.CharField(max_length=22)
+	year = models.IntegerField()
+	file = models.FileField(upload_to='file/')
