4bb110217f379a4ca9364ebe2c770f912c0822d7
Roshan Yadav <djrshn2346@gmail.com>
2019-05-09 18:54:49 +0530

    Search function working

--- results/views.py
+++ results/views.py
@@ -3,7 +3,30 @@ from .models import Results
 
 
 def allresults(request):
-    results = Results.objects
+    results = Results.objects.all()
     mon = request.POST['mahina']
-    yea = request.POST['saal']
-    return render(request, 'results/allresults.html', {'results': results, 'month': mon, 'year': yea})
+    yea = int(request.POST['saal'])
+    pdf_url = {}
+    ti = []
+    da = []
+
+    for i in results:
+        m = i.month
+        y = i.year
+        f = i.file
+        u = f.url
+        t = i.title
+        d = i.date
+        dt = d + " \u2192 " + t
+
+        if (mon == 'all' and yea == y):
+            pdf_url[dt] = u
+
+        elif (mon == m and yea == y):
+            pdf_url[dt] = u
+
+    if (len(pdf_url) == 0):
+        return render(request, 'results/allresults.html', {'y': yea, 'error': 'No Murlis found for this period of time. Try changing the month...'})
+    else:
+        pl = pdf_url.items()
+        return render(request, 'results/allresults.html', {'y': yea, 'pl': pl})
